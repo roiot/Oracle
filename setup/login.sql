e334ab5083353d49ad703106c4ea1d0ac404615e
denis.skryabin <denis.skryabin@google.com>
2016-11-27 15:50:22 +0300

    Use vi in sqlplus

--- setup/login.sql
+++ setup/login.sql
@@ -4,7 +4,10 @@
 # Добавить в ~/.bash_profile: export SQLPATH=/home/oracle/Desktop/Tom_Kyte_Book/scripts/ch00/
 # Для выполнения ~/.bash_profile без перезагрузки системы выполнить: reload .bash_profile from command line: source ~/.bash_profile
 
-define _editor=vi
+define _editor=vi					# для переходв в vi - выполнить ed в коммандной строке sqlplus
+									# вместо vi можно использовать vim
+									# для получения последней выполненной комманды в sqlplus используется комманда l
+
 set serveroutput on size 1000000	# Включение вывода DBMS
 set trimspool on					# Отбрасыввание пробелов при буферизации вывода
 set long 5000						# Байт, отображаемых при выводе CLOB | BLOB
